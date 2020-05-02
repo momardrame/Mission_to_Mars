a3f59d8589cdba0a3c516613c42178e8e8c1dc5a
momardrame <momardrame@yahoo.com>
2020-05-01 21:08:57 -0400

    commit latest code

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
